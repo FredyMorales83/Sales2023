a5f2634d53b12243b90d07efe27b0d55dd2acfe9
Edwin Fredy Morales Morales <isc_morales@live.com.mx>
2023-08-29 20:09:12 -0600

    Seed DB from external API

--- Sales.API/Data/SeedDB.cs
+++ Sales.API/Data/SeedDB.cs
@@ -1,14 +1,19 @@
-﻿using Sales.Shared.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.API.Services;
+using Sales.Shared.Entities;
+using Sales.Shared.Responses;
 
 namespace Sales.API.Data
 {
     public class SeedDB
     {
         private readonly DataContext _context;
+        private readonly IApiService _apiService;
 
-        public SeedDB(DataContext context)
+        public SeedDB(DataContext context, IApiService apiService)
         {
             _context = context;
+            _apiService = apiService;
         }
 
         public async Task SeedAsync()
@@ -21,23 +26,58 @@ namespace Sales.API.Data
         {
             if (!_context.Countries.Any())
             {
-                _context.Countries.Add(new Country
+                Response responseCountries = await _apiService.GetListAsync<CountryResponse>("/v1", "/countries");
+                if (responseCountries.IsSuccess)
                 {
-                    Name = "México",
-                    States = new List<State>() {
-                        new State (){
-                            Name = "Chiapas",
-                            Cities = new List<City>() {
-                                new City { Name = "Tuxtla Gutierrez" },
-                                new City { Name = "Chiapa de Corzo" },
-                                new City { Name = "Tapachula" },
+                    List<CountryResponse> countries = (List<CountryResponse>)responseCountries.Result!;
+                    foreach (CountryResponse countryResponse in countries)
+                    {
+                        Country country = await _context.Countries!.FirstOrDefaultAsync(c => c.Name == countryResponse.Name!)!;
+                        if (country == null)
+                        {
+                            country = new() { Name = countryResponse.Name!, States = new List<State>() };
+                            Response responseStates = await _apiService.GetListAsync<StateResponse>("/v1", $"/countries/{countryResponse.Iso2}/states");
+                            if (responseStates.IsSuccess)
+                            {
+                                List<StateResponse> states = (List<StateResponse>)responseStates.Result!;
+                                foreach (StateResponse stateResponse in states!)
+                                {
+                                    State state = country.States!.FirstOrDefault(s => s.Name == stateResponse.Name!)!;
+                                    if (state == null)
+                                    {
+                                        state = new() { Name = stateResponse.Name!, Cities = new List<City>() };
+                                        Response responseCities = await _apiService.GetListAsync<CityResponse>("/v1", $"/countries/{countryResponse.Iso2}/states/{stateResponse.Iso2}/cities");
+                                        if (responseCities.IsSuccess)
+                                        {
+                                            List<CityResponse> cities = (List<CityResponse>)responseCities.Result!;
+                                            foreach (CityResponse cityResponse in cities)
+                                            {
+                                                if (cityResponse.Name == "Mosfellsbær" || cityResponse.Name == "Șăulița")
+                                                {
+                                                    continue;
+                                                }
+                                                City city = state.Cities!.FirstOrDefault(c => c.Name == cityResponse.Name!)!;
+                                                if (city == null)
+                                                {
+                                                    state.Cities.Add(new City() { Name = cityResponse.Name! });
+                                                }
+                                            }
+                                        }
+                                        if (state.CitiesNumber > 0)
+                                        {
+                                            country.States.Add(state);
+                                        }
+                                    }
+                                }
+                            }
+                            if (country.StatesNumber > 0)
+                            {
+                                _context.Countries.Add(country);
+                                await _context.SaveChangesAsync();
                             }
                         }
                     }
-                });
-                _context.Countries.Add(new Country { Name = "Colombia" });
-                _context.Countries.Add(new Country { Name = "Guatemala" });
-                await _context.SaveChangesAsync();
+                }
             }
         }
     }
